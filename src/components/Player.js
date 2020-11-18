81bfa13c646f2383041a77eeb909a347e3195028
Gastonm1 <gaston.marques@gmail.com>
2020-11-18 15:25:57 -0600

    Lo-Fi Player Updated

---
+++ src/components/Player.js
@@ -0,0 +1,120 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faPlay,
+  faAngleLeft,
+  faAngleRight,
+  faPause,
+} from "@fortawesome/free-solid-svg-icons";
+
+const Player = ({
+  currentSong,
+  setCurrentSong,
+  isPlaying,
+  setIsPlaying,
+  audioRef,
+  songInfo,
+  setSongInfo,
+  songs,
+  setSongs,
+}) => {
+  const activeLibraryHandler = (nextPrev) => {
+    const newSongs = songs.map((song) => {
+      if (song.id === nextPrev.id) {
+        return {
+          ...song,
+          active: true,
+        };
+      } else {
+        return {
+          ...song,
+          active: false,
+        };
+      }
+    });
+    setSongs(newSongs);
+  };
+
+  //Event Handlers
+  const playerSongHandler = () => {
+    if (isPlaying) {
+      audioRef.current.pause();
+      setIsPlaying(!isPlaying);
+    } else {
+      audioRef.current.play();
+      setIsPlaying(!isPlaying);
+    }
+  };
+  const dragHandler = (evt) => {
+    audioRef.current.currentTime = evt.target.value;
+    setSongInfo({ ...songInfo, currentTime: evt.target.value });
+  };
+  //State
+
+  const getTime = (time) => {
+    return (
+      Math.floor(time / 60) + ":" + ("0" + Math.floor(time % 60)).slice(-2)
+    );
+  };
+
+  const skipTrackHandler = async (direction) => {
+    let currentIndex = songs.findIndex((song) => song.id === currentSong.id);
+    if (direction === "next-song") {
+      await setCurrentSong(songs[(currentIndex + 1) % songs.length]);
+      activeLibraryHandler(songs[(currentIndex + 1) % songs.length]);
+    }
+    if (direction === "prev-song") {
+      if ((currentIndex - 1) % songs.length === -1) {
+        await setCurrentSong(songs[songs.length - 1]);
+        activeLibraryHandler(songs[songs.length - 1]);
+        if (isPlaying) {
+          audioRef.current.play();
+        }
+        return;
+      }
+      await setCurrentSong(songs[(currentIndex - 1) % songs.length]);
+      activeLibraryHandler(songs[(currentIndex - 1) % songs.length]);
+    }
+    if (isPlaying) {
+      audioRef.current.play();
+    }
+  };
+
+  return (
+    <div className="player">
+      <div className="time-control">
+        <p>{getTime(songInfo.currentTime)}</p>
+        <input
+          type="range"
+          min={0}
+          max={songInfo.duration || 0}
+          value={songInfo.currentTime}
+          onChange={dragHandler}
+        />
+        <p>{songInfo.duration ? getTime(songInfo.duration) : "0:00"}</p>
+      </div>
+      <div className="play-control">
+        <FontAwesomeIcon
+          onClick={() => skipTrackHandler("prev-song")}
+          className="skip-back"
+          size="2x"
+          icon={faAngleLeft}
+        />
+        <FontAwesomeIcon
+          onClick={playerSongHandler}
+          className="play"
+          size="2x"
+          icon={isPlaying ? faPause : faPlay}
+        />
+        <FontAwesomeIcon
+          onClick={() => skipTrackHandler("next-song")}
+          className="skip-forward"
+          size="2x"
+          icon={faAngleRight}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Player;
---
+++ src/data.js
@@ -0,0 +1,68 @@
+import { v4 as uuidv4 } from "uuid";
+
+function chillHipHop() {
+  return [
+    {
+      name: "Pine and Oak",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/09/2899f7cc22ab12e17d0119819aac3ca9dbab46e6-1024x1024.jpg",
+      artist: "Philanthrope, The Field Tapes",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=10251",
+      color: ["#85553F", "#C84639"],
+      id: uuidv4(),
+      active: true,
+    },
+    {
+      name: "Lemon",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/07/53eef8b1ac9bf40038b2b5dc7ca2d13fc19cf95e-1024x1024.jpg",
+      artist: "Tom Doolie",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=7977",
+      color: ["#6FABCF", "#E15570"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "this again..",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/07/f3dda653e5ff94d787639ef56cb8e922d321a04f-1024x1024.jpg",
+      artist: "SwuM, quickly, quickly",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=8953",
+      color: ["#4B4D4A", "#E7DDC2"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Aqueduct",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/09/6defa6c89a6902fdd84bccd720c2b8fd29c40990-1024x1024.jpg",
+      artist: "Leavv",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=9921",
+      color: ["#386964", "#CB8F65"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Lunar Cycles",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/09/c209a7df7b9bc133dfff73ce86ebc3c57c2b73dd-1024x1024.jpg",
+      artist: "Sleepy Fish",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=10031",
+      color: ["#2A3858", "#DCECEC"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Beaver Creek",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/09/0255e8b8c74c90d4a27c594b3452b2daafae608d-1024x1024.jpg",
+      artist: "Aso, Middle School, Aviino",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=10076",
+      color: ["#174F4A", "#E9885C"],
+      id: uuidv4(),
+      active: false,
+    },
+  ];
+}
+
+export default chillHipHop;
